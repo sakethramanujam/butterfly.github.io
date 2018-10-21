b36b472bd3603983084db4b672a09b55daaa4575
sakethramanujam <ramanujamsamavedam@hotmail.com>
2018-10-21 17:37:36 +0530

    squash one

---
+++ client/src/components/map.js
@@ -0,0 +1,19 @@
+import "mapbox-gl/dist/mapbox-gl.css";
+import MapboxGl from "mapbox-gl/dist/mapbox-gl.js";
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+
+
+const TOKEN =
+  "pk.eyJ1IjoicmFraGkyMTA0IiwiYSI6ImNqbG04aHNrcDEyNG4za2wxc3NsYnhzaGQifQ.bdS6WhHrWfDE11LrybUoNw";
+MapboxGl.accessToken = TOKEN;
+
+
+
+export default class MapComp extends Component {
+    constructor(props) {
+      super(props);
+      this.map = undefined;
+      this.container = undefined;
+      this.tooltip = undefined;
+    }
---
+++ client/src/components/search.js
@@ -0,0 +1,15 @@
+import Searchbar from material-ui-search-bar
+
+
+render(){
+    return(
+    <Searchbar onChange={()=>console.log("changed")}
+    onRequestSearch={()=>console.log("Searching...")}
+    style={
+        {
+        margin: '0 auto',
+        maxWidth:150px;  
+        }
+    }/>
+    )   
+}
