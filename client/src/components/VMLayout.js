95d730abe47c09849c3bd5b44d2c04da5a12ae7c
Saketha Ramanjam <ramanujamsamavedam@hotmail.com>
2018-10-22 07:31:44 +0530

    schemas and barebones

--- client/src/components/VMLayout.js
+++ client/src/components/VMLayout.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Grid, CardContent } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
+import CaseCard from './CaseCard';
+import ReactModal from 'react-modal';
 
 export default class VMLayout extends Component {
 
@@ -18,14 +20,27 @@ export default class VMLayout extends Component {
 
                             <Grid container spacing={24}>
                                 <Grid item xs={6}>
-                                    <Typography color="textSecondary">
+                                    {/* <Typography color="textSecondary">
                                         Vending Machine UI Here...
-                                    </Typography>
+                                    </Typography> */}
+                                    <Button size="small" color="primary">
+                                        Donate
+                                    </Button>
+                                    <Button size="small" color="primary">
+                                        Invest
+                                    </Button>
                                 </Grid>
                                 <Grid item xs={6}>
                                     <Typography color="textSecondary">
-                                        Vending Machine UI Here...
+                                        Projects
                                     </Typography>
+                                    <div className="scroll-handle">
+                                        <CaseCard/>
+                                        <CaseCard/>
+                                        <CaseCard/>
+                                        <CaseCard/>
+                                        <CaseCard/>
+                                    </div>
                                 </Grid>
                             </Grid>
                         </CardContent>
